a6097cdf9bd98300d610bc7546508f083fe07b7c
Matt Kenefick <keneficksays@gmail.com>
2022-11-06 16:39:58 -0500

    .

---
+++ src/index.js
@@ -0,0 +1,59 @@
+import express from 'express';
+import axios from 'axios';
+import { gotScraping } from 'got-scraping';
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+
+// Scraping
+// ---------------------------------------------------------------------------
+
+/**
+ * @param string url
+ * @return object
+ */
+async function scrape(url) {
+	const { body } = await gotScraping({ url });
+
+	const placenames = body.match(/geo\.placename' content='(?<address>[^']+)'/).groups;
+	const placename = placenames.address;
+
+	const prices = body.match(/priced at \$(?<price>[\d\,]+)/).groups;
+	const price = prices.price;
+
+	return {
+		placename, price
+	}
+}
+
+
+// Routes
+// ---------------------------------------------------------------------------
+
+app.get('/', async (req, res) => {
+	try {
+		const url = new URL(req.query.url);
+		// const url = 'https://streeteasy.com/building/180-montague/18e';
+		const content = await scrape(url);
+
+		const csv = `"${content.price}","${content.placename}"`
+
+		res.send(csv);
+	}
+	catch (e) {
+		res.send(e);
+	}
+});
+
+
+// Initialize
+// ---------------------------------------------------------------------------
+
+app.listen(port);
+
+console.log(`
+  ┌─────────────────────────────────────────────────────────────────────────┐
+  │ Listening on http://localhost:3000                                      │
+  └─────────────────────────────────────────────────────────────────────────┘
+`);
